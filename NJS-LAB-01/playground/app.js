6f48222594db68deecf2371d777ca3e531eff37b
Leus Vadym <ipz223_lvo@student.ztu.edu.ua>
2024-03-05 23:41:04 +0200

    NJS-LAB-01 REDUX

--- NJS-LAB-01/playground/app.js
+++ NJS-LAB-01/playground/app.js
@@ -1,58 +1,66 @@
 const yargs = require('yargs');
 const user = require('./user');
 
-const argv = yargs
-  .command('add', 'Додати мову', {
-    title: {
-      demand: true,
-      alias: 't',
-      description: 'Назва мови',
+yargs
+  .command({
+    command: 'add',
+    describe: 'Додати мову',
+    builder: {
+      title: {
+        demandOption: true,
+        alias: 't',
+        description: 'Назва мови',
+      },
+      level: {
+        demandOption: true,
+        alias: 'l',
+        description: 'Рівень володіння',
+      },
     },
-    level: {
-      demand: true,
-      alias: 'l',
-      description: 'Рівень володіння',
+    handler: (argv) => {
+      user.add(argv.title, argv.level);
+      console.log(`Мова "${argv.title}" додана`);
     },
   })
-  .command('remove', 'Видалити мову', {
-    title: {
-      demand: true,
-      alias: 't',
-      description: 'Назва мов',
+  .command({
+    command: 'remove',
+    describe: 'Видалити мову',
+    builder: {
+      title: {
+        demandOption: true,
+        alias: 't',
+        description: 'Назва мови',
+      },
+    },
+    handler: (argv) => {
+      user.remove(argv.title);
+      console.log(`Мова "${argv.title}" видалена`);
     },
   })
-  .command('list', 'Список мов')
-  .command('read', 'Інформація про мову', {
-    title: {
-      demand: true,
-      alias: 't',
-      description: 'Назва мови',
+  .command({
+    command: 'list',
+    describe: 'Список мов',
+    handler: () => {
+      const languages = user.list();
+      console.log('Список мов:');
+      languages.forEach(lang => console.log(`  - ${lang.title} (${lang.level})`));
+    },
+  })
+  .command({
+    command: 'read',
+    describe: 'Інформація про мову',
+    builder: {
+      title: {
+        demandOption: true,
+        alias: 't',
+        description: 'Назва мови',
+      },
+    },
+    handler: (argv) => {
+      const language = user.read(argv.title);
+      console.log(`Інформація про мову: "${language.title}":`);
+      console.log(`  - Рівень: ${language.level}`);
     },
   })
   .help()
   .argv;
-
-try {
-  switch (argv._[0]) {
-    case 'add':
-      user.add(argv.title, argv.level);
-      console.log(`Мова "${argv.title}" додана`);
-      break;
-    case 'remove':
-      user.remove(argv.title);
-      console.log(`Мова "${argv.title}" видалена`);
-      break;
-    case 'list':
-      const languages = user.list();
-      console.log('Список мов:');
-      languages.forEach(lang => console.log(`  - ${lang.title} (${lang.level})`) );
-      break;
-    case 'read':
-      const language = user.read(argv.title);
-      console.log(`Інформація про мову: "${language.title}":`);
-      console.log(`  - Рівень: ${language.level}`);
-      break;
-  }
-} catch (err) {
-  console.error(err.message);
-}
